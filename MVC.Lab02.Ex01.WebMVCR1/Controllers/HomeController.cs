439873085f7e436fd95f139ea867f1c9e4e701c2
ark1987s <arkadiy1987@yandex.ru>
2022-10-09 20:22:55 +0300

    new

---
+++ MVC.Lab02.Ex01.WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using MVC.Lab02.Ex01.WebMVCR1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Lab02.Ex01.WebMVCR1.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: Home
+        /* public ActionResult Index()
+         {
+             return View();
+         }*/
+        public string Index(string hel)
+        {
+            int hour = DateTime.Now.Hour;
+            string Greeting = ModelClass.ModelHello() + ", " + hel;
+            //string res = ExeEnum();
+            string res = ExeStruct();
+            return res;
+        }
+        public string ExeEnum()
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+            string res1 = String.Format("Тип банковского счета {0}", goldAccount);
+            string res2 = String.Format("Тип банковского счета {0}", platinumAccount);
+            string res = res1 + "<p>" + res2; return res;
+        }
+        public string ExeStruct()
+        {
+            BankAccount goldBankAccount;
+            goldBankAccount.accType = AccountType.Checking;
+            goldBankAccount.accBal = (decimal)3200.00;
+            goldBankAccount.accNo = 123;
+            string res = String.Format("Информация о банковском счете: {0}", goldBankAccount);
+            return res;
+        }
+    }
+}
---
+++ MVC.Lab02.Ex01.WebMVCR1/Models/StudyCsharp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Web;
+
+namespace MVC.Lab02.Ex01.WebMVCR1.Models
+{
+    public enum AccountType
+    { 
+        Checking, Deposit
+    }
+    public class StudyCsharp
+    {
+    }
+    public struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+        public override string ToString()
+        {
+            //string res = String.Format("Номер счета {0}, баланс {1}, тип {2}", accNo, accBal, accType);
+            // res;
+            string str = $"Номер счета {accNo}, баланс {accBal}, тип {accType}";
+            return str;           
+        }
+    }
+}
