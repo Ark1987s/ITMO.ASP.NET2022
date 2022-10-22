40fcfb72ff58d6fd7e078172c7c6693f5925b1a8
ark1987s <arkadiy1987@yandex.ru>
2022-10-22 21:31:34 +0300

    *

--- ControlWork/Controllers/HomeController.cs
+++ ControlWork/Controllers/HomeController.cs
@@ -15,20 +15,6 @@ namespace ControlWork.Controllers
             return View(db.Progresses);
         }
 
-        /*        public ActionResult About()
-                {
-                    ViewBag.Message = "Your application description page.";
-
-                    return View();
-                }
-
-                public ActionResult Contact()
-                {
-                    ViewBag.Message = "Your contact page.";
-
-                    return View();
-                }*/
-
         [HttpGet]
         public ActionResult AddStudent()
         {
